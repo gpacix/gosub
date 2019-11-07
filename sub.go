b01a9c5a1e2ce04e3e38ac8cabad0813bc02e6fe
George Paci <george.paci@excella.com>
2019-11-06 23:44:48 -0500

    sub.go: read variables files named in args

--- sub.go
+++ sub.go
@@ -32,27 +32,37 @@ func main() {
 	//fmt.Print(string(dat))
 
 	args := os.Args[1:]
-	fmt.Println("args:", args)
+	//fmt.Println("args:", args)
 	//if len(args) < 1 {
 	//	fmt.Println("WARNING: no variable files specified; no substitution will be done")
 	//}
+	m := map[string]string{"MY_VAR": "my value", "OTHER": "another val"}
+	for _, fn := range args {
+		d, err := ioutil.ReadFile(fn)
+		check(err)
+		lines := strings.Split(string(d), "\n")
+		for _, ln := range lines {
+			kv := splitOnce(ln, "=")
+			if len(kv[0]) > 0 {
+				m[kv[0]] = kv[1]
+			}
+		}
+	}
+	//fmt.Println(m)
 
 	dat, err := ioutil.ReadAll(os.Stdin)
 	check(err)
 
 	s := string(dat)
-	fmt.Printf("%d bytes: %s", len(dat), s)
-
-	m := map[string]string{"MY_VAR": "my value", "OTHER": "another val"}
-	fmt.Println(m)
+	//fmt.Printf("%d bytes: %s", len(dat), s)
 
 	for k, v := range m {
 		r, _ := regexp.Compile("\\$" + k + "\\b")
 		s = r.ReplaceAllString(s, v)
 		r2, _ := regexp.Compile("\\${" + k + "}")
 		s = r2.ReplaceAllString(s, v)
-		fmt.Print(s)
 	}
+	fmt.Print(s)
 
 	//ln := "MY_VAR=eq in my=value"
 	//fmt.Println(splitOnce(ln, "=")[0])
